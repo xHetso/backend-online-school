211dfc402e7616dbb3f8bcc224d9b46e7071d602
Hetso <datacener23491248@gmail.com>
2024-03-24 03:21:09 +0500

    Add CORS configuration to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix('api')
-  await app.listen(4200)
+	const app = await NestFactory.create(AppModule)
+
+	app.setGlobalPrefix('api')
+
+	// Настройка CORS
+	app.enableCors({
+		origin: 'http://localhost:5173',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		allowedHeaders: 'Content-Type, Accept',
+	})
+
+	await app.listen(4200)
 }
 bootstrap()
