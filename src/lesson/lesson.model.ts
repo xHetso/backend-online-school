ebd44b10ed81017c9763c61e8ee9b331324b02e4
Hetso <datacener23491248@gmail.com>
2024-03-27 22:21:44 +0500

    Add LessonModule and related files

---
+++ src/lesson/lesson.model.ts
@@ -0,0 +1,27 @@
+import { Ref, prop } from '@typegoose/typegoose'
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import { AuthorModel } from 'src/author/author.model'
+import { IntensiveModel } from 'src/intensive/intensive.model'
+
+export interface LessonModel extends Base {}
+
+export class LessonModel extends TimeStamps {
+	@prop()
+	poster: string
+	@prop()
+	title: string
+	@prop()
+	description: string
+	@prop({ unique: true })
+	slug: string
+	@prop()
+	videoUrl: string
+	@prop({default: 4.0})
+	rating?: number
+	@prop({ref: () => IntensiveModel})
+	intensives: Ref<IntensiveModel>
+	@prop({ref: () => AuthorModel})
+	authors: Ref<AuthorModel>
+	@prop({ default: false })
+	isSendTelegram?: boolean
+}
