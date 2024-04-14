b396f79be927f54654301a68214adb56488c9a2d
Hetso <datacener23491248@gmail.com>
2024-04-14 19:12:55 +0500

    Implemented ChatHistory

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,41 +1,71 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket, OnGatewayConnection } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { readFile, writeFile } from 'fs/promises';
 
 @WebSocketGateway({ namespace: '/api/chat', cors: true })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('chatToServer')
-  handleMessage(
-    @MessageBody() message: { senderId: string; senderName: string, senderSurname: string, content: string; recipientId: string; recipientName: string; recipientSurname: string },
-    @ConnectedSocket() client: Socket
-  ) {
-    // Обработка и передача сообщения
-    const now = new Date();
-    const formattedDate = now.toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' });
-    const formattedTime = now.toLocaleTimeString('ru-RU', { timeZone: 'Asia/Almaty', hour: '2-digit', minute: '2-digit' });
-
-    const formattedMessage = {
-      date: formattedDate, // Дата в формате ДД.ММ.ГГГГ
-      time: formattedTime, // Время в формате ЧЧ:ММ
-      content: message.content,
-      sender: {
-        userId: message.senderId,
-        name: message.senderName,
-        surname: message.senderSurname,
-      },
-      recipient: {
-        id: message.recipientId,
-        name: message.recipientName,
-        surname: message.recipientSurname
-      },
-    };
-    console.log(formattedMessage)
-    this.server.emit('chatToClient', formattedMessage);
+  async handleConnection(client: Socket) {
+    // Этот метод вызывается при подключении каждого клиента
+    try {
+      const chatHistory = await readFile('chatMessages.json', 'utf8');
+      client.emit('chatHistory', chatHistory); // Отправляем историю чата новому клиенту
+    } catch (error) {
+      console.error('Ошибка при чтении файла:', error);
+    }
   }
-  
-  
 
-  // Можно добавить дополнительные обработчики событий по необходимости
+  @SubscribeMessage('chatToServer')
+async handleMessage(
+  @MessageBody() message: { senderId: string; senderName: string, senderSurname: string, content: string; recipientId: string; recipientName: string; recipientSurname: string },
+  @ConnectedSocket() client: Socket
+) {
+  const now = new Date();
+  const formattedDate = now.toLocaleDateString('ru-RU', { timeZone: 'Asia/Almaty' });
+  const formattedTime = now.toLocaleTimeString('ru-RU', { timeZone: 'Asia/Almaty', hour: '2-digit', minute: '2-digit' });
+
+  const formattedMessage = {
+    date: formattedDate,
+    time: formattedTime,
+    content: message.content,
+    sender: {
+      userId: message.senderId,
+      name: message.senderName,
+      surname: message.senderSurname,
+    },
+    recipient: {
+      id: message.recipientId,
+      name: message.recipientName,
+      surname: message.recipientSurname
+    },
+  };
+
+  try {
+    // Чтение текущего содержимого файла и его парсинг
+    let chatHistory = [];
+    try {
+      const fileContent = await readFile('chatMessages.json', 'utf8');
+      chatHistory = JSON.parse(fileContent);
+    } catch (readError) {
+      if (readError.code !== 'ENOENT') {
+        throw readError; // Если ошибка не связана с отсутствием файла, пробрасываем её дальше
+      }
+      // Если файл не найден, начинаем с пустого массива (chatHistory уже инициализирован как [])
+    }
+
+    // Добавление нового сообщения в историю чата
+    chatHistory.push(formattedMessage);
+
+    // Запись обновлённого массива обратно в файл
+    await writeFile('chatMessages.json', JSON.stringify(chatHistory, null, 2));
+    this.server.emit('chatToClient', formattedMessage);
+  } catch (error) {
+    console.error('Ошибка при записи файла:', error);
+  }
+}
+
+
+  // Здесь можно добавить другие методы, если это необходимо
 }
